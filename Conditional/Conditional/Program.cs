7eddfeaeb1dff2bd853b329d1b362394d5a5af8a
rmc3408 <rmc3408@protonmail.com>
2020-07-01 22:17:03 -0400

    ifelse

---
+++ Conditional/Conditional/Program.cs
@@ -0,0 +1,46 @@
+﻿using System;
+
+namespace Conditional
+{
+    internal class Program
+    {
+        private static void Main(string[] args)
+        {
+            
+            string input = "38";
+            //bool success = int.TryParse(input, out int convertedInput);
+            int temp;
+
+            if (int.TryParse(input, out int convertedInput)) // (success = true)
+            {
+                temp = convertedInput;
+                Console.WriteLine("Sucess convertion");
+            }
+            else
+            {
+                temp = 0;
+                Console.WriteLine("Failed convertion or Wrong Characters");
+            }
+
+
+            if (temp < 2)
+            {
+                Console.WriteLine("Winter");
+            }
+            else if (temp > 25)
+            {
+                Console.WriteLine("Summer");
+            }
+            else
+            {
+                Console.WriteLine("Fall or Spring");
+            }
+
+
+
+
+
+
+        }
+    }
+}
