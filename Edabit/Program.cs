7c386c5514d3f0af5fbb0f0b7d00e5fed23d70c8
rmc3408 <rmc3408@protonmail.com>
2020-07-22 23:41:43 -0400

    edabit

---
+++ Edabit/Program.cs
@@ -0,0 +1,34 @@
+﻿using System;
+
+namespace Edabit
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            System.Console.WriteLine(CreatePhoneNumber(new int[]{1,2,3,4,5,6,7,8,9,0}));
+        }
+        public static string CreatePhoneNumber(int[] numbers)
+        {
+            String.Format("({0}{1}{2}) {3}{4}{5}-{6}{7}{8}{9}", numbers.Select(x=>(object)x).ToArray());
+
+
+            string seq;
+            string phone = "(";
+            for (int i = 0; i < 10; i++)
+            {
+                seq = numbers[i].ToString();
+                if(i == 3){
+                    phone += ") ";
+                }
+                if(i == 6){
+                    phone += "-";
+                }
+                phone += seq;
+            }
+            return phone;
+        }
+
+
+    }
+}
