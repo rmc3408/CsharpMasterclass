2e5f762d8b6ec1e86733a968a1d2f21f84f21729
rmc3408 <rmc3408@protonmail.com>
2020-09-09 11:43:58 -0400

    b

--- Array/Program.cs
+++ Array/Program.cs
@@ -6,14 +6,10 @@ namespace Array
     {
         static void Main(string[] args)
         {
-            //multidimensional Array
-
-            int[,] array2D = new int[,] { { 1, 2, 3 }, { 4, 5, 6 }, { 7, 8, 9 } };
-            Console.WriteLine(array2D[1,1]);
-            Console.WriteLine(array2D[2,0]);
             
             
             
+            //declare and initialize Arrays
             int[] groupX = { 1, 2, 3, 4, 5, 6 };
             int[] groupA = new int[3];
             int[] groupB = new int[] { 10, 20 };
@@ -21,6 +17,48 @@ namespace Array
             groupA[0] = 6;
             groupA[1] = 8;
             groupA[2] = 1;
+
+
+            //Jagged Array
+
+            int[][] jArray = new int[][]
+            {
+                new int[]{1,2,3,4,5},
+                new int[]{10,20, 30, 40, 50},
+                new int[]{7, 11}
+            };
+
+            Console.WriteLine($"The middle second array value is {jArray[1][2]}");
+
+            foreach (int[] item in jArray)
+            {
+                Console.WriteLine($"The value is {item}");
+                foreach (int mini in item)
+                {
+                    Console.WriteLine($"The value is {mini}");
+                }
+
+            }
+
+            
+            string[][] friend = new string[3][]
+            {
+                new string[2]{ "Launa", "Raph" },
+                new string[2]{ "Sonia", "Severino" },
+                new string[2]{ "Maria", "Antonio" }
+            };
+            Random rnd = new Random();
+            Console.WriteLine("\n\nI would like to introduce {0} to {1}"
+                , friend[rnd.Next(3)][rnd.Next(2)], friend[rnd.Next(3)][rnd.Next(2)]);
+
+
+
+            //multidimensional Array
+
+            int[,] array2D = new int[,] { { 1, 2, 3 }, { 4, 5, 6 }, { 7, 8, 9 } };
+            Console.WriteLine(array2D[1, 1]);
+            Console.WriteLine(array2D[2, 0]);
+
 
 
             //Print array and length in C#
