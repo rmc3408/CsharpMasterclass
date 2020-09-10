e27bac0e264569351c1c927f4e918aa3d1adb923
rmc3408 <rmc3408@protonmail.com>
2020-09-10 00:47:46 -0400

    f

--- List/Program.cs
+++ List/Program.cs
@@ -1,4 +1,5 @@
 ﻿using System;
+using System.Collections;
 using System.Collections.Generic;
 using System.Linq;
 using System.Text;
@@ -10,7 +11,41 @@ namespace List
     {
         static void Main(string[] args)
         {
+            int[] score = new int[] { 99, 67, 82, 70 }; //same datatype,  limited number, immutable
             
+            List<int> myL = new List<int> { 1, 2, 3, 4 }; //same datatype, NOT limited size.
+            myL.Add(8);
+            myL.Add(32);
+
+            myL.Sort();
+
+            bool answer = myL.Contains(4);
+
+            int position4 = myL.FindIndex(p => p == 32);
+
+            myL.ForEach(i => Console.WriteLine(i * 2) );
+
+            /// ArrayList
+            ArrayList myAL = new ArrayList();  // Any data type and any size
+            myAL.Add(23);
+            myAL.Add("Ivan");
+            myAL.Add('C');
+            myAL.Add(new Numb() { num = 400 });
+
+            foreach (object item in myAL)
+            {
+                Console.WriteLine(item);
+            }
+            
+        }
+    }
+    class Numb
+    {
+        public int num { get; set; }
+
+        public override string ToString()
+        {
+            return num.ToString();
         }
     }
 }
