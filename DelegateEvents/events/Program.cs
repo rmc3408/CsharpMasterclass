1ab8d60aaaa307b79814690bc972bf74e904f5bd
rmc3408 <rmolina9@my.centennialcollege.ca>
2020-10-10 20:10:54 -0400

    events

---
+++ DelegateEvents/events/Program.cs
@@ -0,0 +1,12 @@
+﻿using System;
+
+namespace events
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Hello World!");
+        }
+    }
+}
